6933a3a2fbfb1450533b2bb8912df337121665b5
Geert van Boxtel <G.J.M.vanBoxtel@gmail.com>
2020-04-15 14:14:58 +0200

    20200415 v0.1.0 Added fwhm() function

---
+++ R/fwhm.R
@@ -0,0 +1,111 @@
+# fwhm.R
+# Copyright (C) 2019 Geert van Boxtel <gjmvanboxtel@gmail.com>
+# Octave signal package:
+# Author: Petr Mikulik (2009)
+# This program is granted to the public domain.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; see the file COPYING. If not, see
+# <https://www.gnu.org/licenses/>.
+#
+# 20200415 Geert van Boxtel          First version for v0.1.0
+#---------------------------------------------------------------------------------------------------------------------------------
+
+#' Full width at half maximum
+#' 
+#' Compute peak full-width at half maximum or at another level of peak maximum for vector or matrix
+#' 
+#' @param x samples at which \code{y} is measured, specified as a vector. I.e.,
+#'   \code{y} is sampled as \code{y[x]}. Default: \code{seq_len(length(y))}.
+#' @param y signal to find the width of. If \code{y} is a matrix, widths of all
+#'   columns are computed.
+#' @param ref reference. Compute the width with reference to:
+#' \describe{
+#'   \item{\code{'max | zero'}}{\code{max(y)}}
+#'   \item{\code{'middle' | 'min'}}{\code{min(y) + max(y)}}
+#'   \item{\code{'absolute'}}{an absolute level of \code{y}}
+#' }
+#' @param level the level at which to compute the width. Deafault: 0.5.
+#' 
+#' @return full width at half maximum, returned as a vector with a length equal
+#'   to the number of columns in \code{y}, or 1 in case of a vector.
+#' 
+#' @examples
+#' x <- seq(-pi, pi, 0.001)
+#' y <- cos(x)
+#' w <- fwhm(x, y)
+#' m <- x[which.max(y)]
+#' f <- m - w/2
+#' t <- m + w/2
+#' plot(x, y, type="l",
+#'      panel.first = {
+#'        usr <- par('usr')
+#'        rect(f, usr[3], t, usr[4], col = rgb(0, 1, 0, 0.4), border = NA)
+#'      })
+#' abline(h = max(y) / 2, lty = 2, col = "gray")
+#' 
+#' @author Original Octave code Petr Mikulik (2009). Port to R by Geert van
+#'   Boxtel \email{G.J.M.vanBoxtel@@gmail.com}.
+#
+#' @export
+
+fwhm <- function (x = seq_len(length(y)), y, ref = c('max', 'zero', 'middle', 'min', 'absolute'), level = 0.5) {
+
+  if (!is.vector(x)) {
+    stop('x must be a vector')
+  }
+  if (length(x) != NROW(y)) {
+    stop('length of x must match length or number of rows in y')
+  }
+  if (is.vector(y)) {
+    y <- as.matrix(y)
+  }
+  if (is.array(y) && length(dim(y)) > 2) {
+    stop('y must be a vector or a matrix')
+  }
+  nc <- ncol(y)
+  ref <- match.arg(ref)
+  if (!is.numeric(level)) {
+    stop('level must be numeric')
+  }
+
+  w <- rep(0L, nc)
+  for (icol in seq_len(nc)) {
+    yy <- y[, icol]
+    ly <- length(yy)
+    if (ref == 'absolute') {
+      yy <- yy - level
+    } else if (ref == 'max' || ref == 'zero') {
+      yy <- yy - level * max(yy)
+    } else if (ref == 'middle' || ref == 'min') {
+      yy <- yy - level * (max(yy) + min(yy))
+    }
+    ind <- which(yy[1:(ly - 1)] * yy[2:ly] <= 0)
+    if (length(ind) >= 2 && yy[ind[1]] > 0) { # must start ascending
+      ind <- ind[2:length(ind)]
+    }
+    mx <- max(yy)
+    imax <- which.max(yy)[1] # protection against constant or (almost) monotonous functions
+    li <- length(ind)
+    if (li >= 2 && imax >= ind[1] && imax <= ind[li]) {
+      ind1 <- ind[1]
+      ind2 <- ind1 + 1
+      xx1 <- x[ind1] - yy[ind1] * (x[ind2] - x[ind1]) / (yy[ind2] - yy[ind1])
+      ind1 <- ind[li]
+      ind2 <- ind1 + 1
+      xx2 <- x[ind1] - yy[ind1] * (x[ind2] - x[ind1]) / (yy[ind2] - yy[ind1])
+      w[icol] <- xx2 - xx1
+    }
+  }
+  w
+}
--- tests/testthat/test_Filter_Analysis_Functions.R
+++ tests/testthat/test_Filter_Analysis_Functions.R
@@ -21,3 +21,55 @@ test_that("freqs() tests are correct", {
   
 })
 
+# -----------------------------------------------------------------------
+# fwhm()
+
+test_that("parameters to fwhm() are correct", {
+  expect_error(fwhm())
+  expect_error(fwhm(1))
+  expect_error(fwhm(1, 2, 3, 4, 5))
+  expect_error(fwhm(array(1)))
+  expect_error(fwhm(1, c(1,2)))
+  expect_error(fwhm(1, array(1:12, dim = c(2, 3, 2))))
+  expect_error(fwhm(1, 2, ref = 'invalid'))
+  expect_error(fwhm(1, 2, level = 'invalid'))
+})
+
+test_that("fwhm() tests are correct", {
+
+  x <- seq(-pi, pi, 0.001)
+  y <- cos(x)
+  expect_that(fwhm(x, y), equals(2 * pi / 3))
+  
+  expect_that(fwhm(y = -10:10), equals(0L))
+  expect_that(fwhm(y = rep(1L, 50)), equals(0L))
+
+  x <- seq(-20, 20, 1)
+  y1 <- -4 + rep(0L, length(x)); y1[4:10] <- 8
+  y2 <- -2 + rep(0L, length(x)); y2[4:11] <- 2
+  y3 =  2 + rep(0L, length(x)); y3[5:13] <- 10
+  expect_that(fwhm(x, cbind(y1, y2, y3)), equals(c(20/3, 7.5, 9.25)))
+
+  x <- 1:3
+  y <- c(-1, 3, -1)
+  expect_that(fwhm(x, y), equals(0.75))
+  expect_that(fwhm(x, y, 'max'), equals(0.75))
+  expect_that(fwhm(x, y, 'zero'), equals(0.75))
+  expect_that(fwhm(x, y, 'middle'), equals(1L))
+  expect_that(fwhm(x, y, 'min'), equals(1L))
+
+  x <- 1:3
+  y <- c(-1, 3, -1)
+  expect_that(fwhm(x, y, level = 0.1), equals(1.35))
+  expect_that(fwhm(x, y, ref = 'max', level = 0.1), equals(1.35))
+  expect_that(fwhm(x, y, ref = 'min', level = 0.1), equals(1.40))
+  expect_that(fwhm(x, y, ref = 'abs', level = 2.5), equals(0.25))
+  expect_that(fwhm(x, y, ref = 'abs', level = -0.5), equals(1.75))
+  
+  x <- -5:5
+  y <- 18 - x * x
+  expect_that(fwhm(y = y), equals(6))
+  expect_that(fwhm(x, y), equals(6))
+  expect_that(fwhm(x, y, 'min'), equals(7))
+  
+})
