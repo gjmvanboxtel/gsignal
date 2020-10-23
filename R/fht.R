e9145bebddb24acc6f1d69966edfef437b16733d
Geert van Boxtel <G.J.M.vanBoxtel@gmail.com>
2020-10-23 17:55:20 +0200

    20201023 v0.1.0 Combined fht() and ifht() functions

--- R/fht.R
+++ R/fht.R
@@ -24,7 +24,7 @@
 
 #' Fast Hartley Transform
 #' 
-#' Compute the Hartley transform of a signal using FFT
+#' Compute the (inverse) Hartley transform of a signal using FFT
 #' 
 #' The Hartley transform is an integral transform closely related to the Fourier
 #' transform, but which transforms real-valued functions to real-valued
@@ -33,7 +33,10 @@
 #' to requiring complex numbers) and of being its own inverse [1].
 #' 
 #' This function implements the Hartley transform by calculating the difference
-#' between the real- and imaginary-valued parts of the Fourier-transformed signal [1].
+#' between the real- and imaginary-valued parts of the Fourier-transformed
+#' signal [1]. The forward and inverse Hartley transforms are the same (except
+#' for a scale factor of 1/N for the inverse Hartley transform), but implemented
+#' using different functions.
 #' 
 #' @param x input data, specified as a numeric vector or matrix. In case of a
 #'   vector it represents a single signal; in case of a matrix each column is a
@@ -41,7 +44,7 @@
 #' @param n transform length, specified as a positive integer scalar. Default:
 #'   \code{NROW(x)}.
 #'  
-#' @return Hartley transform, returned as a vector or matrix.
+#' @return (inverse) Hartley transform, returned as a vector or matrix.
 #'  
 #' @examples
 #' # FHT of a 2.5 Hz signal with offset
@@ -58,13 +61,19 @@
 #' plot(f[1:to], X[1:to], type = "l", xlab = "", ylab = "", main = "Hartley Transform")
 #' par(op)
 #' 
+#' x <- 1:10
+#' X <- fht(x)
+#' all.equal(x, ifht(X))
+#' # [1] TRUE
+#' 
 #' @author Muthiah Annamalai, \email{muthiah.annamalai@@uta.edu}; port to R by
 #'   Geert van Boxtel, \email{G.J.M.vanBoxtel@@gmail.com}.
 #' 
 #' @references [1] \url{https://en.wikipedia.org/wiki/Hartley_transform}
 #' 
-#' @seealso \code{\link{ifht}}, \code{\link{fft}}
-#' 
+#' @seealso \code{\link{fft}}
+#'
+#' @rdname fht 
 #' @export
 
  fht <- function (x, n = NROW(x)) {
@@ -95,3 +104,36 @@
   }
   y
 }
+
+#' @rdname fht
+#' @export
+ 
+ ifht <- function (x, n = NROW(x)) {
+   
+   # check parameters
+   if (!(is.vector(x) || is.matrix(x)) || !is.numeric(x)) {
+     stop('x must be a numeric or vector or matrix')
+   }
+   
+   if (is.vector(x)) {
+     vec <- TRUE
+     x <- as.matrix(x, ncol = 1)
+   } else {
+     vec <- FALSE
+   }
+   nr <- nrow(x)
+   ns <- ncol(x)
+   
+   if(!isPosscal(n) || !isWhole(n)) {
+     stop("n must be a positive integer")
+   }
+   
+   Y <- imvfft(x)
+   y <- Re(Y) + Im(Y)
+   
+   if (vec) {
+     y <- as.vector(y)
+   }
+   y
+ }
+ 
