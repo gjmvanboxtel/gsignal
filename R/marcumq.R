dc693bb528f5344ff412068c3370d3920a9a95b8
Geert van Boxtel <G.J.M.vanBoxtel@gmail.com>
2020-11-23 12:51:02 +0100

    20201123 v0.1.0 Added marcumq() function

---
+++ R/marcumq.R
@@ -0,0 +1,47 @@
+# marcumq.R
+# Copyright (C) 2020 William Asquith <william.asquith at ttu.edu>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# See also: http://www.gnu.org/licenses/gpl-2.0.txt
+#
+# Version history
+# 20201123  GvB       setup for gsignal v0.1.0
+#---------------------------------------------------------------------------------------------------------------------
+
+#' Marcum Q function
+#' 
+#' Compute the generalized Marcum Q function
+#' 
+#' The code for this function was taken from the help file of the
+#' \code{cdfkmu} function in the in the \code{lmomco} package,
+#' based on a suggestion of Daniel Wollschlaeger.
+#'   
+#' @param a,b input arguments, specified as non-negative real numbers.
+#' @param m order, specified as a positive integer
+#' 
+#' @return Marcum Q function.
+#' 
+#' @examples
+#' marcumq(12.4, 12.5)
+#'
+#' @author William Asquith, \email{william.asquith@@ttu.edu}.
+#'   
+#' @references \url{https://cran.r-project.org/package=lmomco}
+#
+#' @export
+
+marcumq <- function(a, b, m = 1) {
+  stats::pchisq(b^2, df = 2 * m, ncp = a^2, lower.tail=FALSE) 
+}
